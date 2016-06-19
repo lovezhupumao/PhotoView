7adca7b0e00b28133d4d48fd0fabbbc2c2e452e1
lovezhupumao <20103177@cqu.edu.cn>
2016-06-19 17:31:05 +0800

    修改了bottombar

--- app/src/main/java/farmer/zpm/com/photoview/HomeActivity.java
+++ app/src/main/java/farmer/zpm/com/photoview/HomeActivity.java
@@ -1,34 +1,139 @@
 package farmer.zpm.com.photoview;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.res.Resources;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import com.ashokvarma.bottomnavigation.BadgeItem;
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
 import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.BottomBarTab;
+import com.roughike.bottombar.OnMenuTabClickListener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import farmer.zpm.com.photoview.com.zpm.photoview.fragment.FifthFragment;
+import farmer.zpm.com.photoview.com.zpm.photoview.fragment.FirstFragment;
+import farmer.zpm.com.photoview.com.zpm.photoview.fragment.FourthFragment;
+import farmer.zpm.com.photoview.com.zpm.photoview.fragment.SecondFragment;
+import farmer.zpm.com.photoview.com.zpm.photoview.fragment.ThirtyFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
-    //    @BindView(R.id.homeactivity_bottom_navigation_bar)
+    @BindView(R.id.homeactivity_bottom_navigation_bar)
     BottomNavigationBar mBottomNavigationBar;
     @BindView(R.id.homeactivity_toolbar)
     Toolbar toolbar;
-
+    @BindView(R.id.homeactivity_framelayout)
+    FrameLayout mFrameLayout;
+    private BottomBar mBottomBar;
+    private FirstFragment firstFragment;
+    private SecondFragment secondFragment;
+    private ThirtyFragment thirtyFragment;
+    private FourthFragment fourthFragment;
+    private FifthFragment fifthFragment;
+    private FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
-        mBottomNavigationBar = (BottomNavigationBar) findViewById(R.id.homeactivity_bottom_navigation_bar);
-        setSupportActionBar(toolbar);
+        fragmentManager=getFragmentManager();
+        initBottomBar(savedInstanceState);
+        //initBottomBar();
+
+
+
+    }
+
+    private void initBottomBar(Bundle savedInstanceState) {
+        mBottomBar = BottomBar.attach(this, savedInstanceState);
+        mBottomBar.noTabletGoodness();
+        mBottomBar.setItems(R.menu.bottombar_menu);
+        mBottomBar.setOnMenuTabClickListener(new OnMenuTabClickListener() {
+            @Override
+            public void onMenuTabSelected(@IdRes int menuItemId) {
+                FragmentTransaction ft=fragmentManager.beginTransaction();
+                hideFragments(ft);
+                switch (menuItemId){
+                    case R.id.bb_menu_recents:
+                        toolbar.setBackgroundColor(getResources().getColor(R.color.orange));
+                        if (firstFragment==null) {
+                            firstFragment = new FirstFragment();
+                            ft.add(R.id.homeactivity_framelayout, firstFragment);
+                        }
+                        else {
+                            ft.show(firstFragment);
+                        }
+                        break;
+                    case R.id.bb_menu_favorites:
+                        toolbar.setBackgroundColor(getResources().getColor(R.color.teal));
+                        if (secondFragment == null) {
+                            secondFragment = new SecondFragment();
+                            ft.add(R.id.homeactivity_framelayout, secondFragment);
+                        } else {
+                            ft.show(secondFragment);
+                        }
+                        break;
+                    case R.id.bb_menu_nearby:
+                        toolbar.setBackgroundColor(getResources().getColor(R.color.blue));
+                        if (thirtyFragment == null) {
+                            thirtyFragment = new ThirtyFragment();
+                            ft.add(R.id.homeactivity_framelayout, thirtyFragment);
+                        } else {
+                            ft.show(thirtyFragment);
+                        }
+                    case R.id.bb_menu_friends:
+                        toolbar.setBackgroundColor(getResources().getColor(R.color.brown));
+                        if (fourthFragment == null) {
+                            fourthFragment = new FourthFragment();
+                            ft.add(R.id.homeactivity_framelayout, fourthFragment);
+                        } else {
+                            ft.show(fourthFragment);
+                        }
+                        break;
+                    case R.id.bb_menu_food:
+                        toolbar.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                        if (fifthFragment == null) {
+                            fifthFragment = new FifthFragment();
+                            ft.add(R.id.homeactivity_framelayout, fifthFragment);
+                        } else {
+                            ft.show(fifthFragment);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                ft.commit();
+            }
+
+            @Override
+            public void onMenuTabReSelected(@IdRes int menuItemId) {
+
+            }
+        });
+        mBottomBar.mapColorForTab(0, getResources().getColor(R.color.orange));
+        mBottomBar.mapColorForTab(1, getResources().getColor(R.color.teal));
+        mBottomBar.mapColorForTab(2, getResources().getColor(R.color.blue));
+        mBottomBar.mapColorForTab(3, getResources().getColor(R.color.blue));
+        mBottomBar.mapColorForTab(4, getResources().getColor(R.color.colorAccent));
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mBottomBar.onSaveInstanceState(outState);
+    }
+
+    private void initBottomBar() {
         mBottomNavigationBar.setTabSelectedListener(new BottomNavigationBar.OnTabSelectedListener() {
             @Override
             public void onTabSelected(int position) {
@@ -84,10 +189,18 @@ public class HomeActivity extends AppCompatActivity {
                         .setActiveColorResource(R.color.colorAccent))
                 .setFirstSelectedPosition(0)
                 .initialise();
-
-
-
     }
-
+    public void hideFragments(FragmentTransaction ft) {
+        if (firstFragment != null)
+            ft.hide(firstFragment);
+        if (secondFragment != null)
+            ft.hide(secondFragment);
+        if (thirtyFragment != null)
+            ft.hide(thirtyFragment);
+        if (fourthFragment != null)
+            ft.hide(fourthFragment);
+        if (fifthFragment!=null)
+            ft.hide(fifthFragment);
+    }
 
 }
