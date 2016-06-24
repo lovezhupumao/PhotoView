7d640a16094b640237bc9ae87b3ff826feb8509e
lovezhupumao <20103177@cqu.edu.cn>
2016-06-24 09:48:35 +0800

    引入超时

--- app/src/main/java/farmer/zpm/com/photoview/BaseActivity.java
+++ app/src/main/java/farmer/zpm/com/photoview/BaseActivity.java
@@ -1,7 +1,9 @@
 package farmer.zpm.com.photoview;
 
+import android.app.Activity;
+
 /**
  * Created by Administrator on 2016/6/18 0018.
  */
-public class BaseActivity {
+public class BaseActivity extends Activity {
 }
