159979a7e7ec8ede9528e599bb86fea05e17bfbd
ZhuPumao <20103177@cqu.edu.cn>
2016-07-27 20:18:12 +0800

    增加sharesdk

--- app/src/main/java/farmer/zpm/com/photoview/com/zpm/photoview/fragment/FifthFragment.java
+++ app/src/main/java/farmer/zpm/com/photoview/com/zpm/photoview/fragment/FifthFragment.java
@@ -6,20 +6,62 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 import farmer.zpm.com.photoview.R;
 
 /**
  * Created by Administrator on 2016/6/19.
  */
 public class FifthFragment extends Fragment {
+
     private View view;
+    @BindView(R.id.weibo)
+    ImageView mImageView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         if (view==null)
             view=inflater.inflate(R.layout.fragment_home_fifty,container,false);
-
+        ButterKnife.bind(this,view);
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(),"hello",Toast.LENGTH_LONG).show();
+                showShare();
+            }
+        });
         return view;
+    }
+    private void showShare() {
+        ShareSDK.initSDK(getActivity());
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间等使用
+        oks.setTitle("标题");
+        // titleUrl是标题的网络链接，QQ和QQ空间等使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
+
+// 启动分享GUI
+        oks.show(getActivity());
     }
 }
