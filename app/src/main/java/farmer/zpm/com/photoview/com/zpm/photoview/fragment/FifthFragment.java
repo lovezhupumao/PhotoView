2abb6681516fb18ad6f027305d8782acd3a1dc23
ZhuPumao <20103177@cqu.edu.cn>
2016-07-28 22:37:58 +0800

    实现了微博分享

--- app/src/main/java/farmer/zpm/com/photoview/com/zpm/photoview/fragment/FifthFragment.java
+++ app/src/main/java/farmer/zpm/com/photoview/com/zpm/photoview/fragment/FifthFragment.java
@@ -9,10 +9,18 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.mob.tools.utils.UIHandler;
+
+import java.util.HashMap;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import cn.sharesdk.framework.Platform;
+import cn.sharesdk.framework.PlatformActionListener;
+import cn.sharesdk.framework.PlatformDb;
 import cn.sharesdk.framework.ShareSDK;
 import cn.sharesdk.onekeyshare.OnekeyShare;
+import cn.sharesdk.sina.weibo.SinaWeibo;
 import farmer.zpm.com.photoview.R;
 
 /**
@@ -33,7 +41,8 @@ public class FifthFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getActivity(),"hello",Toast.LENGTH_LONG).show();
-                showShare();
+//
+                login();
             }
         });
         return view;
@@ -63,5 +72,66 @@ public class FifthFragment extends Fragment {
 
 // 启动分享GUI
         oks.show(getActivity());
+    }
+    public void login(){
+        ShareSDK.initSDK(getActivity());
+        Platform weibo = ShareSDK.getPlatform(SinaWeibo.NAME);
+        weibo.setPlatformActionListener(new PlatformActionListener() {
+            @Override
+            public void onComplete(Platform platform, int action, HashMap<String, Object> res) {
+                if (action == Platform.ACTION_USER_INFOR) {
+                    PlatformDb platDB = platform.getDb();//获取数平台数据DB
+                    //通过DB获取各种数据
+                    platDB.getToken();
+                    platDB.getUserGender();
+                    platDB.getUserIcon();
+                    platDB.getUserId();
+                    platDB.getUserName();
+                }
+
+                String id,name,description,profile_image_url;
+                id=res.get("id").toString();//ID
+                name=res.get("name").toString();//用户名
+                description=res.get("description").toString();//描述
+                profile_image_url=res.get("profile_image_url").toString();//头像链接
+                String str="ID: "+id+";\n"+
+                        "用户名： "+name+";\n"+
+                        "描述："+description+";\n"+
+                        "用户头像地址："+profile_image_url;
+                System.out.println("用户资料: "+str);
+            }
+
+            @Override
+            public void onError(Platform platform, int i, Throwable throwable) {
+
+            }
+
+            @Override
+            public void onCancel(Platform platform, int i) {
+
+            }
+        });
+        weibo.authorize();
+
+    }
+    private void authorize(Platform plat) {
+        if (plat == null) {
+//            popupOthers();
+            return;
+        }
+//判断指定平台是否已经完成授权
+        if(plat.isAuthValid()) {
+            String userId = plat.getDb().getUserId();
+            if (userId != null) {
+//                UIHandler.sendEmptyMessage(MSG_USERID_FOUND, this);
+//                login(plat.getName(), userId, null);
+                return;
+            }
+        }
+//        plat.setPlatformActionListener(this);
+        // true不使用SSO授权，false使用SSO授权
+        plat.SSOSetting(true);
+        //获取用户资料
+        plat.showUser(null);
     }
 }
